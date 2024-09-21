fa12165985806e9ae06eb6b56ec2e98a63067e0a
Usman Khalid <usmankhld602@gmail.com>
2024-09-21 00:12:23 -0500

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
 } from "react-router-dom";
-import { Home, NurseForm, NurseOrders } from "./Pages";
+import { DoctorOrders, Home, NurseForm, NurseOrders } from "./Pages";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/nurse-form" element={<NurseForm />} />
-          <Route path="/nurse-orders" element={<NurseOrders />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/nurse-form" element={<NurseForm />} />
+					<Route path="/nurse-orders" element={<NurseOrders />} />
+					<Route path="/doctor-orders" element={<DoctorOrders />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+	return (
+		<div className="w-screen h-screen bg-black flex items-center justify-center">
+			<div className="h-96 w-72 bg-white">
+				<button>Doctor</button>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
