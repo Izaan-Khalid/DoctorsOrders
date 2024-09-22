9407afe39e228d19bf94716a307551f3b2c221ec
Izaan Khalid <izaan.khalid@outlook.com>
2024-09-21 22:35:42 -0500

    NurseOrders Logout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import "./Home.css"
-import AuthButtons from "./AuthButtons"
+import AuthButtons from "../AuthButtons"
 import { useAuth0 } from "@auth0/auth0-react"
 
 export const Home = () => {
